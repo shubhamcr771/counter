d4ba6fe8dac68e4db52f1b1e1fb6ad8cf0627790
Shubham <shubhamsingh705010@gmail.com>
2023-09-30 12:47:57 +0530

    js

---
+++ app.js
@@ -0,0 +1,5 @@
+let count = 0;
+const value = document.getElementById("value");
+const decrease = document.querySelector(".btn-decrease");
+const reset = document.querySelector(".btn-reset");
+const increase = document.querySelector(".btn-increase");
