6adff99b6ebe92be4fe9a73657d1234b2c116490
Shubham <shubhamsingh705010@gmail.com>
2023-10-02 16:00:04 +0530

    complete

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ btn.addEventListener('click', function(e){
     else{
         count = 0;
     }
+    if(count > 0){
+        value.style.color = 'green';
+    }
+    else if(count < 0){
+        value.style.color = 'red';
+    }
+    else{
+        value.style.color = 'black';
+    }
     value.textContent = count;
     
     
