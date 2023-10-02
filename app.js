79652a81c6b3fcd0739950c318ac6b2d775810f3
Shubham <shubhamsingh705010@gmail.com>
2023-10-02 16:04:24 +0530

    js inpo

--- app.js
+++ app.js
@@ -3,37 +3,26 @@ let count = 0;
 
 // select value and buttons
 
-const value = document.querySelector('#value');
-const btns = document.querySelectorAll('.btn');
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
 
-btns.forEach(function(btn){
-btn.addEventListener('click', function(e){
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
-    if(styles.contains('decrease')) {
-        count--;
-        
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
     }
-    else if(styles.contains('increase')){
-        count++;
-    }
-    else{
-        count = 0;
-    }
-    if(count > 0){
-        value.style.color = 'green';
-    }
-    else if(count < 0){
-        value.style.color = 'red';
-    }
-    else{
-        value.style.color = 'black';
+    if (count > 0) {
+      value.style.color = "green";
+    } else if (count < 0) {
+      value.style.color = "red";
+    } else {
+      value.style.color = "black";
     }
     value.textContent = count;
-    
-    
-    
-    
-    
-
-});
+  });
 });
